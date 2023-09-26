658b9a035ec07337aa0ad2bad5afac0e74fdcffe
Karl MARQUES <kmarques@vetixy.com>
2023-09-26 11:37:00 +0200

    BIN1: Scrapper JSON/Web

---
+++ scripts/csv-utiliy/lib.js
@@ -0,0 +1,18 @@
+String.prototype.decodeCSV = function ({delimiter = ";"} = {}) {
+  const [headersLine, ...valuesLines] = this.split(/\r?\n/);
+  const headers = headersLine.split(";");
+  return valuesLines.map((line) => {
+    const values = line.split(delimiter);
+    return values.reduce((acc, item, index) => {
+      acc[headers[index]] = item;
+      return acc;
+    }, {});
+  });
+};
+
+Array.prototype.encodeCSV = function ({delimiter = ";"} = {}) {
+    const headers = Object.keys(this.at(0)).join(delimiter);
+    const values = this.map(item => Object.values(item).join(delimiter));
+
+    return `${headers}\n${values.join('\n')}`;
+}
