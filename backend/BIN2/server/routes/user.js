06881f59e9f25a6ebc87a4e398176744fe0f9a59
Karl MARQUES <karl@kmarques.dev>
2023-11-10 17:04:04 +0100

    BIN2: Auth

--- backend/BIN2/server/routes/user.js
+++ backend/BIN2/server/routes/user.js
@@ -1,43 +1,51 @@
 const { Router } = require("express");
-const { ValidationError } = require("sequelize");
 const User = require("../models/User");
+const checkAuth = require("../middlewares/checkAuth");
 const router = new Router();
 
-router.get("/users", async (req, res, next) => {
+router.get("/users", checkAuth({ transient: true }), async (req, res, next) => {
+  if (req.user) {
+    req.query.id = req.user.id;
+  }
   const users = await User.findAll({
     where: req.query,
   });
   res.json(users);
 });
 
+router.use(checkAuth());
+
 router.post("/users", async (req, res, next) => {
   try {
     res.status(201).json(await User.create(req.body));
   } catch (error) {
-    if (error instanceof ValidationError) {
-      res.status(422).json({});
-    }
+    next(error);
   }
 });
 
 router.get("/users/:id", async (req, res, next) => {
+  if (req.user.id !== parseInt(req.params.id)) return res.sendStatus(403);
   const user = await User.findByPk(parseInt(req.params.id));
   if (!user) res.sendStatus(404);
   else res.json(user);
 });
 
 router.put("/users/:id", async (req, res, next) => {
-  const result = await User.destroy({
-    where: {
+  try {
+    const result = await User.destroy({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    const user = await User.create({
+      ...req.body,
       id: parseInt(req.params.id),
-    },
-  });
-  const user = await User.create({
-    ...req.body,
-    id: parseInt(req.params.id),
-  });
+    });
 
-  res.status(result ? 200 : 201).json(user);
+    res.status(result ? 200 : 201).json(user);
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.delete("/users/:id", async (req, res, next) => {
@@ -48,6 +56,25 @@ router.delete("/users/:id", async (req, res, next) => {
   });
 
   res.sendStatus(result ? 204 : 404);
+});
+
+router.patch("/users/:id", async (req, res, next) => {
+  try {
+    const [nbUpdated] = await User.update(req.body, {
+      where: {
+        id: parseInt(req.params.id),
+      },
+      individualHooks: true,
+    });
+    const user = await User.findByPk(parseInt(req.params.id));
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
--- backend/BIN2/server/server.js
+++ backend/BIN2/server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 3000;
-const userRouter = require('./routes/user');
+const userRouter = require("./routes/user");
+const { ValidationError } = require("sequelize");
+const User = require("./models/User");
 const app = express();
 
 //function parseBody(req, res, next) {
@@ -47,7 +51,47 @@ app.patch("/", (req, res, next) => {
   res.send("Hello World! from PATCH" + JSON.stringify(req.body));
 });
 
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+  if (!user) return res.sendStatus(401);
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.sendStatus(401);
+
+  res.json({
+    token: jwt.sign(
+      {
+        userId: user.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2 days",
+      }
+    ),
+  });
+});
+
 app.use(userRouter);
+
+app.use((error, req, res, next) => {
+  if (error instanceof ValidationError) {
+    console.log(error);
+    return res.status(422).json(/* TODO */ {});
+    /**
+     * Exemple:
+     * {
+     *    "email": [
+     *        "Is not an email",
+     *        "Cannot be empty"
+     *    ]
+     * }
+     */
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
